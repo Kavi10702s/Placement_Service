0433845cdf4cb1fa9407a8317dcf70d2840933d4
Kavi10702s <125440412+Kavi10702s@users.noreply.github.com>
2023-02-23 18:55:29 +0530

    Add files via upload

---
+++ placementservice1/src/main/java/com/tns/placementproject/Placementservice1Application.java
@@ -0,0 +1,13 @@
+package com.tns.placementproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Placementservice1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Placementservice1Application.class, args);
+	}
+
+}
